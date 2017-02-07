a472e704b39cf8fd834a8bfcda41f46d9a869493
Ken Mazaika <ken@thefirehoseproject.com>
2017-02-07 13:35:17 -0800

    Fix broken photo uploading.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,8 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
+  has_many :photos
 
-  mount_uploader :photo, PhotoUploader
-  
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
